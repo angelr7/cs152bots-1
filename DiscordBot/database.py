358bf90281c656786c6871b41fe1737078525289
angelr7 <angeljruiz2016@gmail.com>
2022-03-08 17:20:25 -0800

    fix

--- DiscordBot/database.py
+++ DiscordBot/database.py
@@ -83,8 +83,9 @@ def update_resolution(db, action, mod_msg_id):
 def check_report(db, reported_msg_id):
      cursor = db.cursor()
      cursor.execute(
-          """SELECT COUNT(DISTINCT reporter) FROM reports_table WHERE original_msg_id = ?;""",
-          (reported_msg_id)
+          f"""SELECT COUNT(DISTINCT reporter) 
+          FROM reports_table 
+          WHERE original_msg_id = {reported_msg_id};"""
      )
      result = cursor.fetchall()[0][0]
      db.commit()
